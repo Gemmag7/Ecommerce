bb5a1f44592c04a111851c3b1454db69f64a67f5
Gemmag7 <72978574+Gemmag7@users.noreply.github.com>
2023-02-15 17:11:19 +0000

    inserting data into database from backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require ('express'); 
-const bodyParser= require('body-parser');
 const app = express();
 const morgan = require('morgan')
 const mongoose = require('mongoose');
@@ -9,26 +8,59 @@ require('dotenv/config');
 const api = process.env.API_URL; 
 
 //middleware
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id:1, 
-    name:'hair dresser  5', 
-    image:'some_url',
+
+const productSchema = mongoose.Schema({
+  name: String, 
+  image: String, 
+  countInStock:  Number
+   
+})
+
+const Product = mongoose.model('Product', productSchema); 
+
+app.get(`${api}/products`, async(req, res) => {
+   const productList = await Product.find();
+ 
+  if(!productList){
+    res.status(500).json({
+      sucess: false
+    })
   }
-  res.send(product);
+  res.send(productList);
 })
 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-console.log(req.body);
-  console.log(newProduct);
-  res.send(newProduct);
+  const product = new Product({
+    name: req.body.name, 
+    image: req.body.image, 
+    countInStock: req.body.countInStock
+  })
+  
+
+  console.log(JSON.stringify(req.body));
+  product.save().then((createdProduct => {
+    res.status(201).json(createdProduct)
+    console.log(JSON.stringify(createdProduct));
+  })).catch((err) => {
+    res.status(500).json({
+      error: err, 
+      success: false
+    })
+    console.log(err);
+  })
 })
 
-mongoose.connect(process.env.CONNECTION_STRING).then(() =>{
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  dbName: 'eshop-database'
+}).then(() =>{
   console.log('Database connection successful');
 })
 .catch((err) =>{
